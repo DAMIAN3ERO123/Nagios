16676140cf2f028dbf2d200b16aa9348b18da79b
DAMIAN3ERO123 <36737516+DAMIAN3ERO123@users.noreply.github.com>
2022-06-16 12:51:08 -0500

    Update redUp.sh

--- redUp.sh
+++ redUp.sh
@@ -7,7 +7,7 @@ echo "Ejemplo 192.168.100.0/24"
 
 read red
 
-#if test `echo $red | grep ^[0-255].[0-255].[0-255].[0-255][/][1-30]?`
+#if test `echo $red | grep -E '^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$'`
 
 mascara=`echo $red | cut -f 2 -d '/'`
 mascara=`expr 32 - $mascara`
@@ -19,20 +19,73 @@ ips=`expr $ips - 2`
 ini_red=`echo $red | cut -f 4 -d '.' | cut -f 1 -d '/'`
 ini_red=`expr $ini_red + 1`
 #echo $ini_red
-date
+
 for (( i=$ini_red ; i<=$ips ; i++ ))
 do 
     ip=`echo $red | cut -f 1,2,3 -d '.'`
     ip=`echo $ip.$i`
-    #cho $ip
-    if test `ping -c 1 $ip | grep received | cut -d ',' -f 2 | cut -d ' ' -f 2` -ne 0
+    #echo $ip
+    validando=`ping -c 4 $ip`
+    resultado=`echo $validando | cut -d ',' -f 2 | cut -d ' ' -f 2`
+    #echo $resultado
+    if test $resultado -ne 0
     then 
         echo $ip >> listaAlcanzables.txt
     fi
 done  
 
+echo ""
 echo "Lista de IPs Alcanzables"
+
 cat listaAlcanzables.txt
+echo ""
+
+opcion=0
+while test `echo $opcion -ne 3`
+do
+    echo "Ingresa 1 para agregar la/las IPs al monitoreo de hosts"
+    echo "Ingresa 2 para revisar que servicios tienen activos en las IPs alcanzables"
+    echo "Ingresa 3 para salir"
+
+    read opcion
+
+    if test $opcion -eq 1
+    then 
+        clear
+        ip_opcion=0
+        while test `echo $ip_opcion -ne 3` 2> /dev/null
+        do
+            echo "Ingresa 1 para agregar todas las IPs encontradas al monitoreo"
+            echo "Ingresa 2 para agregar una sola IP"
+            echo "Ingresa 3 para volver al menu anterior"
+            read ip_opcion
+        
+            if test $ip_opcion -eq 1 2> /dev/null
+            then
+                echo "Estoy agregando las IPs al Monitoreo..."
+            elif test $ip_opcion -eq 2 2> /dev/null
+            then
+                echo "Escrbe el numero de IP a Monitorear"
+                read ip
+                if test `echo $ip | grep -E '^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$'`
+                then
+                    echo "Voy a agregar la IP $ip al Monitoreo.."
+                else 
+                    echo "El numero de IP es invalido"
+                fi
+            elif test $ip_opcion -eq 3
+            then 
+                echo "Regrensando al menu anterior"
+                sleep 3
+                clear
+            else
+                echo "La opcion  o IP es Invalida, intentalo de nuevo"
+            fi
+      
+        done
+    fi
+done
+
 
 rm listaAlcanzables.txt
 
